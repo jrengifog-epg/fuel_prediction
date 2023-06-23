2f05cc5b44f9b59cd4b5cfacda130562a795667d
Razthany <razthany15@gmail.com>
2023-06-23 14:51:08 -0500

    subien salida

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n!a=qv1njuuqqmyvkr9z021d_kposw_239!c!dk4ciikycfwx#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "localhost", "localhost:5173"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api',
 ]
 
